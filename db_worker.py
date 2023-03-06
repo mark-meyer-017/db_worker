8e19ebb5e5e7ad084ee67a8523f3acf87b5021e3
amarkevich <shade.jdm@yandex.ru>
2023-03-06 10:07:17 +0300

    Refactoring

--- db_worker.py
+++ db_worker.py
@@ -1,10 +1,9 @@
-import sqlite3 as sql
 import os
+import sqlite3 as sql
 
-DB_WORKER = None
 
+class DBWorker:
 
-class DBworker:
     def __init__(self, db_name):
         self.db_name = db_name
 
@@ -15,29 +14,14 @@ class DBworker:
             result_list = [x[0] for x in cursor.fetchall()]
         return result_list
 
-    def create(self):
-        name = check_name_created_table()
-        columns_data = ""
-        while True:
-            columns_data += check_create_column(columns_data)
-            print(f"{'='*21}\n"
-                  f"Добавить еще колонку?\n"
-                  f"да  : 1\n"
-                  f"нет : 0")
-            continue_choice = num_input_check(2)
-            if continue_choice == 1:
-                continue
-            if continue_choice == 0:
-                break
+    def create_table(self, name, columns_data):
         with sql.connect(self.db_name) as connection:
             cursor = connection.cursor()
             cursor.execute(f"""CREATE TABLE IF NOT EXISTS {name}  (
             {columns_data[0:-1]}
             )""")
 
-    def delete(self):
-        print(f"Список доступных баз {self.list_tables()}")
-        name = check_name("Введите имя удаляемой таблицы:")
+    def delete(self, name):
         with sql.connect(self.db_name) as connection:
             cursor = connection.cursor()
             cursor.execute(f"""DROP TABLE IF EXISTS {name}""")
@@ -61,7 +45,7 @@ def get_db_name():
     return db_name
 
 
-def get_user_choice(worker):
+def user_choice_handler(worker: DBWorker):
     text = f"{'='*39}\n" \
            f"Для получения списка таблиц введите : 1\n" \
            f"Для добавления таблицы введите      : 2\n" \
@@ -73,9 +57,13 @@ def get_user_choice(worker):
     if choice == 1:
         print(worker.list_tables())
     if choice == 2:
-        return worker.create()
+        name = check_name_created_table(worker)
+        columns_data = check_columns_data()
+        worker.create_table(name, columns_data)
     if choice == 3:
-        return worker.delete()
+        print(f"Список доступных баз {worker.list_tables()}")
+        name = check_name("Введите имя удаляемой таблицы:")
+        worker.delete(name)
     if choice == 4:
         pass
     if choice == 0:
@@ -96,7 +84,7 @@ def num_input_check(end_range, start_range=0):
 def check_name(text):
     while True:
         name = str(input(f"{text}\n")).strip()
-        if name == "":
+        if not name:
             print("Строка не должна быть пустой")
             continue
         if name[0].isdigit():
@@ -106,13 +94,13 @@ def check_name(text):
 
 
 def check_create_column(column_data):
-    COLUMN_TYPES = {1: "INTEGER", 2: "TEXT"}
-    COLUMN_KEYWORDS = {1: "PRIMARY KEY", 2: "NOT NULL", 3: "DEFAULT"}
+    column_types = {1: "INTEGER", 2: "TEXT"}
+    column_keywords = {1: "PRIMARY KEY", 2: "NOT NULL", 3: "DEFAULT"}
     column_name = check_name("Введите имя новой колонки:")
     print(f"Выберите тип колонки.\n"
           f"INTEGER : 1\n"
-          f"TEXT: 2")
-    column_type = COLUMN_TYPES[num_input_check(3, 1)]
+          f"TEXT    : 2")
+    column_type = column_types[num_input_check(3, 1)]
     keywords = set()
     default_value = ""
     while True:
@@ -120,12 +108,12 @@ def check_create_column(column_data):
               "PRIMARY KEY : 1\n"
               "NOT NULL    : 2\n"
               "DEFAULT     : 3")
-        column_keyword = COLUMN_KEYWORDS[num_input_check(4, 1)]
-        if column_keyword == COLUMN_KEYWORDS[1] and column_keyword in \
+        column_keyword = column_keywords[num_input_check(4, 1)]
+        if column_keyword == column_keywords[1] and column_keyword in \
                 column_data:
-            print("В таблице может быть только один ключ.")
+            print("В таблице может быть только один основной ключ.")
             continue
-        if column_keyword == COLUMN_KEYWORDS[3]:
+        if column_keyword == column_keywords[3]:
             print("Введите значение по умолчанию.")
             default_value = f" {input()}"
         keywords.add(column_keyword)
@@ -139,28 +127,44 @@ def check_create_column(column_data):
             break
     keys = ""
     for key in keywords:
-        if key == COLUMN_KEYWORDS[3]:
+        if key == column_keywords[3]:
             keys += f"{key} {default_value} "
         else:
             keys += f"{key} "
     return f"{column_name} {column_type} {keys},"
 
 
-def check_name_created_table():
+def check_name_created_table(worker):
+    if not isinstance(worker, DBWorker):
+        raise ValueError("Incorrect worker type.")
     while True:
         name = check_name("Введите имя новой таблицы:")
-        if name in DB_WORKER.list_tables():
-            print(f"Список таблиц: {DB_WORKER.list_tables()}\n"
+        if name in worker.list_tables():
+            print(f"Список таблиц: {worker.list_tables()}\n"
                   f"Таблица с таким именем уже существует.")
             continue
         return name
 
 
-def main():
-    global DB_WORKER
-    DB_WORKER = DBworker(get_db_name())
+def check_columns_data():
+    columns_data = ""
     while True:
-        get_user_choice(DB_WORKER)
+        columns_data += check_create_column(columns_data)
+        print(f"{'=' * 21}\n"
+              f"Добавить еще колонку?\n"
+              f"да  : 1\n"
+              f"нет : 0")
+        continue_choice = num_input_check(2)
+        if continue_choice == 1:
+            continue
+        if continue_choice == 0:
+            return columns_data
+
+
+def main():
+    db_worker = DBWorker(get_db_name())
+    while True:
+        user_choice_handler(db_worker)
 
 
 if __name__ == '__main__':
